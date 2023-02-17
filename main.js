25c032db6d6d2bf0a876f2784ba169d7b7bcf0da
33marvs33 <108669397+33marvs33@users.noreply.github.com>
2023-02-18 01:10:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     const date = new Date();
-    const hour = date.getHours()
+    let hour = date.getHours();
+    let min = date.getMinutes();
     const dateYear = document.getElementById('dateTime');
     const time =document.getElementById('time');
     const greeting = document.getElementById('greeting');
+    
+    hour < 10 ? '0'+hour : hour
+    min < 10 ? '0'+min : min
+    hour < 10 ? '0'+hour : hour
     dateYear.innerHTML = `
         ${date.getMonth()+1}:${date.getDate()}:${date.getFullYear()}
     `
     time.innerHTML = `
-        ${hour}:${date.getMinutes()}
+        ${hour}:${min}
     `
     if(hour >= 0 && hour <= 11){
         greeting.innerHTML = "GoodMorning"
