6811cbf381adf0204aa3d79f09480da6a88037a7
33marvs33 <108669397+33marvs33@users.noreply.github.com>
2023-02-18 01:50:01 +0800

    Update main.js
    update the time to be updated every second using setInterval and made changes

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    const dateYear = document.getElementById('dateTime');
-    const time =document.getElementById('time');
-    const greeting = document.getElementById('greeting');
-    
-    setInterval(() => {
-     hour < 10 ? '0'+hour : hour
-    min < 10 ? '0'+min : min
-    sec < 10 ? '0'+sec : sec
-    dateYear.innerHTML = `
-        ${date.getMonth()+1}:${date.getDate()}:${date.getFullYear()}
-    `
-    time.innerHTML = `
-        ${hour}:${min}:${sec}
-    `
-    if(hour >= 0 && hour <= 11){
-        greeting.innerHTML = "GoodMorning"
-    }else if(hour >= 12 && hour <= 17) {
-        greeting.innerHTML = "GoodAfternoon"
-    }else{
-        greeting.innerHTML = "GoodEvening"
-    }
-    
-    },1000)
-   
+setInterval(() => {
+  const date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let year = date.getFullYear();
+  const dateYear = document.getElementById("dateTime");
+  const time = document.getElementById("time");
+  const greeting = document.getElementById("greeting");
 
-})
+  //   if(hour<10) {
+  //     hour = '0'+hour
+  //   }
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+
+  dateYear.innerHTML = `
+        ${month}:${day}:${year}
+    `;
+  time.innerHTML = `
+        ${hour}:${min}:${sec}
+    `;
+  if (hour >= 0 && hour <= 11) {
+    greeting.innerHTML = "GoodMorning";
+  } else if (hour >= 12 && hour <= 17) {
+    greeting.innerHTML = "GoodAfternoon";
+  } else {
+    greeting.innerHTML = "GoodEvening";
+  }
+}),
+  1000;
